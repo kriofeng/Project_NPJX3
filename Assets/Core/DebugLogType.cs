de46c834782356d11fa8eec8224f25ee9b7542cc
ZhuYingTK <100524163+ZhuYingTK@users.noreply.github.com>
2024-01-26 19:20:54 +0800

    删除不需要的类

--- Assets/Core/DebugLogType.cs
+++ Assets/Core/DebugLogType.cs
@@ -4,8 +4,4 @@ using UnityEngine;
 
 public class DebugLogType
 {
-    public static bool AttackInfo = true;
-    public static bool TreateInfo = true;
-    public static bool BuffInfo = true;
-    public static bool SkillInfo = true;
 }
